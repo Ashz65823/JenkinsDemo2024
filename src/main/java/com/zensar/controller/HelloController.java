11571ed7a8b4071de7e7c6af5a15548fac7e62ac
Ashwini AM <ashwini.am@zensar.com>
2024-05-30 16:44:49 +0530

    Commit

--- src/main/java/com/zensar/controller/HelloController.java
+++ src/main/java/com/zensar/controller/HelloController.java
@@ -30,6 +30,6 @@ public class HelloController {
 	
 	@GetMapping("/gm")
 	public String goodMorning() {
-		return"<h1> Happy Morning All !!!!!</h1>";	
+		return"<h1> Happy Morning All !!!!</h1>";	
 	}
 }
