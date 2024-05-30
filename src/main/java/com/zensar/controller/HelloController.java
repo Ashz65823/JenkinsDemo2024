bef785fc6482f32f1aeba795bfb9fc253e37307d
Ashwini AM <ashwini.am@zensar.com>
2024-05-30 16:17:30 +0530

    removed and added git

---
+++ src/main/java/com/zensar/controller/HelloController.java
@@ -0,0 +1,35 @@
+package com.zensar.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+	@GetMapping("/hi")
+	public String sayHi() {
+		return"<h1> Welcome to Jenkins and say Hi.........</h1>";	
+	}
+	
+	@GetMapping("/hello")
+	public String sayHello() {
+		return"<h1> Welcome to Jenkins </h1>";	
+	}
+	
+	@GetMapping("/bye")
+	public String sayBye() {
+		return"<h1> Bye....!!! Jenkins ;-) </h1>";	
+	}
+	
+	@GetMapping("/welcome")
+	public String start() {
+		return"<h1> Welcome !!!!!</h1>";	
+	}
+	
+	@GetMapping("/gm")
+	public String goodMorning() {
+		return"<h1> Happy Morning All !!!!!</h1>";	
+	}
+}
